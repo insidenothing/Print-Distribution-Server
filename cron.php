16fcc5ef954a46ed724842afb7dd181756a2cfb3
Patrick McGuire <insidenothing@gmail.com>
2010-12-22 08:32:41 -0800

    start log

--- cron.php
+++ cron.php
@@ -1,5 +1,6 @@
 <?
 // this file, run by a cron job, will scan folders for .ps and .pcl files and ftp them to the internal printer's ip
 
+error_log("[".date('h:iA n/j/y')."]  [scanning folders] \n", 3, '/printDirect/logs/cron.log');
 
 ?>
