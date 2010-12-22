343f2d4d828bdcaa52f4f8e5685d180258129852
Patrick McGuire <insidenothing@gmail.com>
2010-12-22 10:15:48 -0800

    send everything from 190 to all staff

--- cron.php
+++ cron.php
@@ -12,6 +12,7 @@ error_log(date('r')." WARNING: Couldn't change ftp directory for $ip $file. \n",
 }
 if (ftp_put($conn_id, $remote_file, $file, FTP_BINARY)) {
 $last_line = system('rm -f '.$file, $retval);
+error_log("[".date('h:iA n/j/y')."]  [printed] [$ip] [$file] \n", 3, '/printDirect/logs/cron.log');
 //error_log(date('r')." NOTICE: $log printed successfully. \n", 3, '/logs/printer.log');
 } else {
 error_log(date('r')." ERROR: There was a problem while uploading $ip $file. \n", 3, '/printDirect/logs/printer.log');
@@ -29,7 +30,12 @@ function processIP($directory,$ip){
         // if $file isn't this directory or its parent,
         // add it to the results array
         if ($file != '.' && $file != '..' && $file != 'CVS')
-          pushProcess($directory.'/'.$file,$file,$ip); 
+          //pushProcess($directory.'/'.$file,$file,$ip); 
+          pushProcess($directory.'/'.$file,$file,'192.168.100.190'); 
+          pushProcess($directory.'/'.$file,$file,'192.168.100.153'); 
+          pushProcess($directory.'/'.$file,$file,'192.168.100.173'); 
+          pushProcess($directory.'/'.$file,$file,'192.168.100.149'); 
+          pushProcess($directory.'/'.$file,$file,'192.168.100.164'); 
 error_log("[".date('h:iA n/j/y')."]  [processing] [$directory] [$file] \n", 3, '/printDirect/logs/cron.log');
 
     }
@@ -40,7 +46,7 @@ error_log("[".date('h:iA n/j/y')."]  [processing] [$directory] [$file] \n", 3, '
 
 
 processIP('/printDirect/192.168.100.190','192.168.100.190');
-processIP('/printDirect/192.168.100.153','192.168.100.153');
+//processIP('/printDirect/192.168.100.153','192.168.100.153');
 
 
 error_log("[".date('h:iA n/j/y')."]  [scan complete] \n", 3, '/printDirect/logs/cron.log');
