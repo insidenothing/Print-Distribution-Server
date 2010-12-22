4e3afd3e9bc74e61e546f42d641b9e6d290d8167
Patrick McGuire <insidenothing@gmail.com>
2010-12-22 08:56:11 -0800

    scan folders

--- cron.php
+++ cron.php
@@ -3,4 +3,33 @@
 
 error_log("[".date('h:iA n/j/y')."]  [scanning folders] \n", 3, '/printDirect/logs/cron.log');
 
+
+
+function processIP($directory){
+   // create an array to hold directory list
+    $results = array();
+    // create a handler for the directory
+    $handler = opendir($directory);
+    // keep going until all files in directory have been read
+    while ($file = readdir($handler)) {
+        // if $file isn't this directory or its parent,
+        // add it to the results array
+        if ($file != '.' && $file != '..' && $file != 'CVS')
+           
+error_log("[".date('h:iA n/j/y')."]  [processing] [$directory] [$file] \n", 3, '/printDirect/logs/cron.log');
+
+    }
+    // tidy up: close the handler
+    closedir($handler);
+    // done!
+}
+
+
+processIP('/printDirect/192.168.100.190');
+processIP('/printDirect/192.168.100.153');
+
+
+error_log("[".date('h:iA n/j/y')."]  [scan complete] \n", 3, '/printDirect/logs/cron.log');
+
+
 ?>
